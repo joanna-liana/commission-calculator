59944247dcf6ddcb84ae4fc61512f3ce75eaeb5c
Joanna Liana <joanna_liana@outlook.com>
2022-05-09 16:38:34 +0200

    Update transaction client notes

--- src/commission/domain/transaction-client/ITransactionClient.ts
+++ src/commission/domain/transaction-client/ITransactionClient.ts
@@ -1,12 +1,8 @@
 import { Euro } from '../money/Euro';
 
+// read model used for various policies/rules
 export interface ITransactionClient {
   id: number;
   isVIP: boolean;
   monthlyTurnover?: Euro;
-}
-
-// TODO: make it a proper aggregate
-export class TransactionClient {
-  private constructor(public readonly id: number) {}
 }
